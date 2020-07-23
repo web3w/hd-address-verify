43dbd6093fe3bde20ca4663d7968c23f01c31a2f
Tom Wang <wang69258@gmail.com>
2020-07-23 19:03:39 +0800

    add BCH_TEST LTC_TEST

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ module.exports = {
     "BTC": (address) => btcSeriesVerify(address, "BTC"),
     "BTC_TEST": (address) => btcSeriesVerify(address, "BTC", "TEST"),
     "BCH": (address) => btcSeriesVerify(address, "BCH"),
+    "BCH_TEST": (address) => btcSeriesVerify(address, "BCH","TEST"),
     "LTC": (address) => btcSeriesVerify(address, "LTC"),
+    "LTC_TEST": (address) => btcSeriesVerify(address, "LTC","TEST"),
     "ETH": (address) => ethVerify(address, "ETH"),
     "TRX": (address) => trxVerify(address, "TRX"),
 }
